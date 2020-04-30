06eb1675867543f6d8ecb9b9c2ffc93a4d9fbb7f
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-29 22:33:16 -0600

    CORS Working

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+//fetch polyfill
+class App extends Component {
+  getUsers(){
+    fetch('http://localhost:5000/api/users', {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    }).then(response => response.json())
+    .then(data => console.log(data))
+  }
+    render(){  
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          {this.getUsers()}
+
+        </a>
+      </header>
+    </div>
+  );
+  };
+}
+
+export default App;
