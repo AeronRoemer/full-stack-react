dcf90b0ee0b8405b21bc1db2338dd17cc37c1586
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-06-02 19:16:11 -0600

    API Call for All Courses

--- client/src/components/AllCourses.js
+++ client/src/components/AllCourses.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
+//filter with search
+class CoursesDisplay extends Component { 
+  state= {
+    query: '',
+    courses: '',
+  }
 
-export default ({context}) => { 
-  console.log(context.actions.getAllCourses())
+  updateQuery = (query) => {
+    this.setState(() => ({
+        query: query.trim()
+    }))
+  }
+
+  componentDidMount(){
+    this.props.context.actions.getAllCourses()
+    .then((data)=>{
+      this.setState(() => ({
+        courses: data
+      }))
+    })
+  }
+  render(){
+    let allCourses = this.state.courses
+    // allCourses.map(item => (
+    //  <p key={item.id}>{item.title}</p>
+    //))
   return (
   <div className="bounds">
     <div className="grid-100">
       Courses to appear here
-      {/*
-        context.actions.getAllCourses()
-    .then(data => data.map(item => (
-      <p key={item.id}>{item.title}</p>
-    ))) 
-    */}
+      {console.log(allCourses[0], this.state.courses)}
     </div>
   </div>
   );
+  }
 }
+
+export default CoursesDisplay
 
